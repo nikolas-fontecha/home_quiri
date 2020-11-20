1ea1f0cf58d288800ace71c1ea40b6f66640bde1
nicolas alejandro fontecha pardo <nafp_927@MacBook-Pro-de-nicolas.local>
2020-11-19 20:26:16 -0500

    proyecto IoT

--- washer-start/functions/index.js
+++ washer-start/functions/index.js
@@ -102,36 +102,148 @@ exports.faketoken = functions.https.onRequest((request, response) => {
 const app = smarthome();
 
 app.onSync((body) => {
-  // TODO: Implement full SYNC response
-  return {};
+  return {
+    requestId: body.requestId,
+    payload: {
+      agentUserId: USER_ID,
+      devices: [{
+        id: 'washer',
+        type: 'action.devices.types.WASHER',
+        traits: [
+          'action.devices.traits.OnOff',
+          'action.devices.traits.StartStop',
+          'action.devices.traits.RunCycle',
+        ],
+        name: {
+          defaultNames: ['My Washer'],
+          name: 'Washer',
+          nicknames: ['Washer'],
+        },
+        deviceInfo: {
+          manufacturer: 'Acme Co',
+          model: 'acme-washer',
+          hwVersion: '1.0',
+          swVersion: '1.0.1',
+        },
+        willReportState: true,
+        attributes: {
+          pausable: true,
+        },
+      }],
+    },
+  };
 });
+
+
 
 const queryFirebase = async (deviceId) => {
   const snapshot = await firebaseRef.child(deviceId).once('value');
   const snapshotVal = snapshot.val();
-  // TODO: Define device states to return
-  return {};
+  return {
+    on: snapshotVal.OnOff.on,
+    isPaused: snapshotVal.StartStop.isPaused,
+    isRunning: snapshotVal.StartStop.isRunning,
+  };
 };
 
-// eslint-disable-next-line
 const queryDevice = async (deviceId) => {
   const data = await queryFirebase(deviceId);
-  // TODO: Define device states to return
-  return {};
+  return {
+    on: data.on,
+    isPaused: data.isPaused,
+    isRunning: data.isRunning,
+    currentRunCycle: [{
+      currentCycle: 'rinse',
+      nextCycle: 'spin',
+      lang: 'en',
+    }],
+    currentTotalRemainingTime: 1212,
+    currentCycleRemainingTime: 301,
+  };
 };
 
-app.onQuery((body) => {
-  // TODO: Implement QUERY response
-  return {};
+
+
+app.onQuery(async (body) => {
+  const {requestId} = body;
+  const payload = {
+    devices: {},
+  };
+  const queryPromises = [];
+  const intent = body.inputs[0];
+  for (const device of intent.payload.devices) {
+    const deviceId = device.id;
+    queryPromises.push(queryDevice(deviceId)
+        .then((data) => {
+        // Add response to device payload
+          payload.devices[deviceId] = data;
+        }
+        ));
+  }
+  // Wait for all promises to resolve
+  await Promise.all(queryPromises);
+  return {
+    requestId: requestId,
+    payload: payload,
+  };
 });
 
 const updateDevice = async (execution, deviceId) => {
-  // TODO: Add commands to change device states
+  const {params, command} = execution;
+  let state; let ref;
+  switch (command) {
+    case 'action.devices.commands.OnOff':
+      state = {on: params.on};
+      ref = firebaseRef.child(deviceId).child('OnOff');
+      break;
+    case 'action.devices.commands.StartStop':
+      state = {isRunning: params.start};
+      ref = firebaseRef.child(deviceId).child('StartStop');
+      break;
+    case 'action.devices.commands.PauseUnpause':
+      state = {isPaused: params.pause};
+      ref = firebaseRef.child(deviceId).child('StartStop');
+      break;
+  }
+
+  return ref.update(state)
+      .then(() => state);
 };
 
-app.onExecute((body) => {
-  // TODO: Implement EXECUTE response
-  return {};
+app.onExecute(async (body) => {
+  const {requestId} = body;
+  // Execution results are grouped by status
+  const result = {
+    ids: [],
+    status: 'SUCCESS',
+    states: {
+      online: true,
+    },
+  };
+
+  const executePromises = [];
+  const intent = body.inputs[0];
+  for (const command of intent.payload.commands) {
+    for (const device of command.devices) {
+      for (const execution of command.execution) {
+        executePromises.push(
+            updateDevice(execution, device.id)
+                .then((data) => {
+                  result.ids.push(device.id);
+                  Object.assign(result.states, data);
+                })
+                .catch(() => functions.logger.error('EXECUTE', device.id)));
+      }
+    }
+  }
+
+  await Promise.all(executePromises);
+  return {
+    requestId: requestId,
+    payload: {
+      commands: [result],
+    },
+  };
 });
 
 app.onDisconnect((body, headers) => {
@@ -145,9 +257,18 @@ exports.smarthome = functions.https.onRequest(app);
 exports.requestsync = functions.https.onRequest(async (request, response) => {
   response.set('Access-Control-Allow-Origin', '*');
   functions.logger.info(`Request SYNC for user ${USER_ID}`);
-
-  // TODO: Call HomeGraph API for user '123'
-  response.status(500).send(`Request SYNC not implemented`);
+  try {
+    const res = await homegraph.devices.requestSync({
+      requestBody: {
+        agentUserId: USER_ID,
+      },
+    });
+    functions.logger.info('Request sync response:', res.status, res.data);
+    response.json(res.data);
+  } catch (err) {
+    functions.logger.error(err);
+    response.status(500).send(`Error requesting sync: ${err}`);
+  }
 });
 
 /**
@@ -155,9 +276,30 @@ exports.requestsync = functions.https.onRequest(async (request, response) => {
  * has been changed.
  */
 exports.reportstate = functions.database.ref('{deviceId}').onWrite(
-    async (change, context) => {
-      functions.logger.info('Firebase write event triggered Report State');
+  async (change, context) => {
+    functions.logger.info('Firebase write event triggered Report State');
+    const snapshot = change.after.val();
 
-      // TODO: Get latest state and call HomeGraph API
+    const requestBody = {
+      requestId: 'ff36a3cc', /* Any unique ID */
+      agentUserId: USER_ID,
+      payload: {
+        devices: {
+          states: {
+            /* Report the current state of our washer */
+            [context.params.deviceId]: {
+              on: snapshot.OnOff.on,
+              isPaused: snapshot.StartStop.isPaused,
+              isRunning: snapshot.StartStop.isRunning,
+            },
+          },
+        },
+      },
+    };
+
+    const res = await homegraph.devices.reportStateAndNotification({
+      requestBody,
+    });
+    functions.logger.info('Report state response:', res.status, res.data);
     });
 
